4dcc72b8a7bb1fd8da0cb6f4ac8cd012e31eb1be
tejasvi raj <tejasviraj8686@gmail.com>
2023-12-07 12:41:01 +0530

    header top

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,29 +1,35 @@
 import React from "react";
 import "./Header.scss";
-import { Link } from "react-router-dom";
+import HeaderBG from "../../assets/header-bg.png";
+import Headerlogo from "../../assets/headerlogo.png";
 import fbicon from "../../assets/icons/facebook.png";
 import yticon from "../../assets/icons/youtube.png";
 import igicon from "../../assets/icons/instagram.png";
 
 const Header = () => {
   return (
-    <div className="top-bar">
-      <div className="top-links">
-        <Link to="/contactus">
-          <span>Help Desk &nbsp;&nbsp;</span>
-        </Link>
-      </div>
+    <div class="header-container">
+      <div className="header-logo-icon">
+        <div className="header-logo">
+          <div className="header-bg-img">
+            <img src={HeaderBG} alt="" />
+          </div>
+          <div className="header-logo-bg">
+            <img src={Headerlogo} alt="" />
+          </div>
+        </div>
 
-      <div className="top-icons">
-        <a href="https://www.facebook.com" target="_blank">
-          <img src={fbicon} width={"17vw"} alt="" />
-        </a>
-        <a href="https://www.youtube.com" target="_blank">
-          <img src={yticon} width={"17vw"} alt="" />
-        </a>
-        <a href="https://www.instagram.com" target="_blank">
-          <img src={igicon} width={"17vw"} alt="" />
-        </a>
+        <div className="header-icon">
+          <a href="https://www.facebook.com" target="_blank">
+            <img src={fbicon} width={"18.5vw"} alt="" />
+          </a>
+          <a href="https://www.youtube.com" target="_blank">
+            <img src={yticon} width={"18.5vw"} alt="" />
+          </a>
+          <a href="https://www.instagram.com" target="_blank">
+            <img src={igicon} width={"18.5vw"} alt="" />
+          </a>
+        </div>
       </div>
     </div>
   );
